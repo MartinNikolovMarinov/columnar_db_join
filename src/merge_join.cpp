cc03a51723b6eb7b86e277944d34b4880c3f369a
Martin Marinov <martin_n_marinov@protonmail.com>
2024-02-19 21:31:54 +0200

    merge join bug fixed

--- src/merge_join.cpp
+++ src/merge_join.cpp
@@ -4,88 +4,108 @@
 
 namespace dbms {
 
+// TODO: Move this to dbms and re-use it in other joins.
+void appendRowToResult(JoinResult& result,
+                       const ColumnNames& leftColNames,
+                       const ColumnNames& rightColNames,
+                       const ColumnGroup& left,
+                       const ColumnGroup& right,
+                       u64 leftRowIdx, u64 rightRowIdx) {
+
+    u64 startOfValuesInLeft = leftColNames.colNames.size();
+    u64 startOfValuesInRight = rightColNames.colNames.size();
+    u64 startOfValuesInResult = result.names.colNames.size();
+
+    const auto& writeOrder = createTableWriteOrder(leftColNames, rightColNames, result.names);
+
+    // Append the matching rows to the result.
+    for (auto& [resultIdx, leftIdx] : writeOrder.first) {
+        auto lds = left[leftIdx].data();
+        result.columns[resultIdx].append(lds[leftRowIdx]);
+    }
+    for (auto& [resultIdx, rightIdx] : writeOrder.second) {
+        auto rds = right[rightIdx].data();
+        result.columns[resultIdx].append(rds[rightRowIdx]);
+    }
+
+    u64 valueWriteIdx = startOfValuesInResult;
+    for (u64 i = startOfValuesInLeft; i < left.size(); i++) {
+        auto lds = left[i].data();
+        result.columns[valueWriteIdx].append(lds[leftRowIdx]);
+        valueWriteIdx++;
+    }
+    for (u64 i = startOfValuesInRight; i < right.size(); i++) {
+        auto rds = right[i].data();
+        result.columns[valueWriteIdx].append(rds[rightRowIdx]);
+        valueWriteIdx++;
+    }
+}
+
+bool checkSecondaryKeys(const ColumnNames& leftColNames,
+                        const ColumnNames& rightColNames,
+                        const ColumnGroup& left,
+                        const ColumnGroup& right,
+                        u64 leftRowIdx, u64 rightRowIdx) {
+
+    bool match = true;
+    const auto& leftToRightTranslationTable = createIndexTranslationTable(leftColNames, rightColNames);
+
+    // Check if all of the secondary indices match.
+    for (u64 i = 1; i < leftToRightTranslationTable.size(); i++) {
+        const auto& [leftColIdx, rightColIdx] = leftToRightTranslationTable[i];
+        auto ldataCurr = left[leftColIdx].data();
+        auto rdataCurr = right[rightColIdx].data();
+        if (ldataCurr[leftRowIdx] != rdataCurr[rightRowIdx]) {
+            match = false;
+            break;
+        }
+    }
+
+    return match;
+}
+
 JoinResult mergeJoin(const ColumnGroup& left,
                      const ColumnGroup& right,
                      const ColumnNames& leftColNames,
                      const ColumnNames& rightColNames) {
 
     JoinResult result = JoinResult::createFromNames(leftColNames, rightColNames);
-    const auto& writeOrder = createTableWriteOrder(leftColNames, rightColNames, result.names);
     auto leftToRightTranslationTable = createIndexTranslationTable(leftColNames, rightColNames);
 
     constexpr u64 joinIdxLeft = 0;
     constexpr u64 joinIdxRight = 0;
     u64 iLeft = 0; // row index in left table
     u64 iRight = 0; // row index in right table
-    u64 nLeft = left[0].data().size(); // number of rows in left table
-    u64 nRight = right[0].data().size(); // number of rows in right table
-    std::optional<u64> firstMatchInGroup;
+    u64 nLeft = left[joinIdxLeft].data().size(); // number of rows in left table
+    u64 nRight = right[joinIdxRight].data().size(); // number of rows in right table
 
-    u64 startOfValuesInLeft = leftColNames.colNames.size();
-    u64 startOfValuesInRight = rightColNames.colNames.size();
-    u64 startOfValuesInResult = result.names.colNames.size();
+    u64 endOfGroup = 0;
 
     while (iLeft < nLeft && iRight < nRight) {
-        auto ldata = left[joinIdxLeft].data();
-        auto rdata = right[joinIdxRight].data();
-        u64 a = ldata[iLeft];
-        u64 b = rdata[iRight];
+        auto joinLeftDataSrc = left[joinIdxLeft].data();
+        auto joinRightDataSrc = right[joinIdxRight].data();
+        u64 a = joinLeftDataSrc[iLeft];
+        u64 b = joinRightDataSrc[iRight];
 
         if (a < b) {
-            if (firstMatchInGroup.has_value()) {
-                iRight = firstMatchInGroup.value();
-                firstMatchInGroup.reset();
-            }
             iLeft++;
         }
         else if (a > b) {
-            if (firstMatchInGroup.has_value()) {
-                iRight = firstMatchInGroup.value();
-                firstMatchInGroup.reset();
-            }
-            iRight++;
+            iRight = (iRight < endOfGroup) ? (endOfGroup) : (iRight + 1);
         }
         else {
-            bool match = true;
-
-            // Check if all of the secondary indices match.
-            for (auto& [leftColIdx, rightColIdx] : leftToRightTranslationTable) {
-                auto ldataCurr = left[leftColIdx].data();
-                auto rdataCurr = right[rightColIdx].data();
-                if (ldataCurr[iLeft] != rdataCurr[iRight]) {
-                    match = false;
-                    break;
+            u64 k = iRight;
+            while (k < nRight && joinRightDataSrc[k] == a) {
+                bool matchOnSecondaryKeys = checkSecondaryKeys(leftColNames, rightColNames, left, right, iLeft, k);
+                if (matchOnSecondaryKeys) {
+                    appendRowToResult(result, leftColNames, rightColNames, left, right, iLeft, k);
                 }
+                k++;
             }
 
-            if (match) {
-                // Append the matching rows to the result.
-                for (auto& [resultIdx, leftIdx] : writeOrder.first) {
-                    auto lds = left[leftIdx].data();
-                    result.columns[resultIdx].append(lds[iLeft]);
-                }
-                for (auto& [resultIdx, rightIdx] : writeOrder.second) {
-                    auto rds = right[rightIdx].data();
-                    result.columns[resultIdx].append(rds[iRight]);
-                }
+            endOfGroup = std::max(endOfGroup, k);
 
-                u64 valueWriteIdx = startOfValuesInResult;
-                for (u64 i = startOfValuesInLeft; i < left.size(); i++) {
-                    auto lds = left[i].data();
-                    result.columns[valueWriteIdx].append(lds[iLeft]);
-                    valueWriteIdx++;
-                }
-                for (u64 i = startOfValuesInRight; i < right.size(); i++) {
-                    auto rds = right[i].data();
-                    result.columns[valueWriteIdx].append(rds[iRight]);
-                    valueWriteIdx++;
-                }
-            }
-
-            if (!firstMatchInGroup.has_value()) {
-                firstMatchInGroup = iRight;
-            }
-            iRight++;
+            iLeft++;
         }
     }
 
--- tests/t-merge_join.cpp
+++ tests/t-merge_join.cpp
@@ -11,7 +11,7 @@ i32 mergeJoinOnTwoColumnGroups() {
         u64 expectedSumSquared;
     };
 
-    constexpr const u64 N = 2;
+    constexpr const u64 N = 3;
     std::vector<TestCase> tests (N);
 
     {
@@ -68,7 +68,7 @@ i32 mergeJoinOnTwoColumnGroups() {
         tests[1].leftColNames.valueColNames = { "value" };
 
         tests[1].right = {
-            dbms::Column { std::vector<u64>{1, 1, 1, 2, 3, 3, 3} },
+            dbms::Column { std::vector<u64>{1, 1, 1, 1, 2, 3, 3} },
             dbms::Column { std::vector<u64>{1, 1, 2, 2, 3, 5, 5} },
             dbms::Column { std::vector<u64>{17, 18, 19, 20, 21, 22, 22} },
             dbms::Column { std::vector<u64>{23, 24, 25, 26, 27, 28, 28} },
@@ -105,10 +105,46 @@ i32 mergeJoinOnTwoColumnGroups() {
         tests[1].expectedSumSquared = 10371;
     }
 
+    {
+        tests[2].left = {
+            dbms::Column { std::vector<u64>{ 1, 1, 1, 1, 2, 2, 3, 3, 3} },
+            dbms::Column { std::vector<u64>{ 10, 11, 12, 13, 14, 15, 16, 17, 18} },
+            dbms::Column { std::vector<u64>{ 4, 4, 5, 6, 6, 6, 4, 5, 5} },
+            dbms::Column { std::vector<u64>{ 14, 14, 14, 14, 14, 14, 14, 14, 14} },
+        };
+        tests[2].leftColNames.colNames = { "A", "B", "E" };
+        tests[2].leftColNames.valueColNames = { "value" };
+
+        tests[2].right = {
+            dbms::Column { std::vector<u64>{ 1, 1, 1, 2, 2, 3, 3} },
+            dbms::Column { std::vector<u64>{ 4, 4, 5, 6, 6, 3, 4} },
+            dbms::Column { std::vector<u64>{ 19, 20, 21, 22, 23, 24, 25} },
+            dbms::Column { std::vector<u64>{ 26, 27, 28, 29, 30, 31, 32} },
+            dbms::Column { std::vector<u64>{ 33, 34, 35, 36, 37, 38, 39} },
+        };
+        tests[2].rightColNames.colNames = { "A", "E", "G", "H" };
+        tests[2].rightColNames.valueColNames = { "value" };
+
+        tests[2].expected = dbms::JoinResult {
+            dbms::ColumnGroup {
+                dbms::Column { std::vector<u64>{ 1, 1, 1, 1, 1, 2, 2, 2, 2, 3 } },
+                dbms::Column { std::vector<u64>{ 10, 10, 11, 11, 12, 14, 14, 15, 15, 16 } },
+                dbms::Column { std::vector<u64>{ 4, 4, 4, 4, 5, 6, 6, 6, 6, 4 } },
+                dbms::Column { std::vector<u64>{ 19, 20, 19, 20, 21, 22, 23, 22, 23, 25 } },
+                dbms::Column { std::vector<u64>{ 26, 27, 26, 27, 28, 29, 30, 29, 30, 32 } },
+                dbms::Column { std::vector<u64>{ 14, 14, 14, 14, 14, 14, 14, 14, 14, 14 } },
+                dbms::Column { std::vector<u64>{ 33, 34, 33, 34, 35, 36, 37, 36, 37, 39 } },
+            },
+            dbms::ColumnNames { { "A", "B", "E", "G", "H" }, { "Column1", "Column2" } },
+        };
+        tests[2].expectedSumSquaredCol = dbms::Column {
+            std::vector<u64>{2209, 2304, 2209, 2304, 2401, 2500, 2601, 2500, 2601, 2809}
+        };
+        tests[2].expectedSumSquared = 24438;
+    }
+
     for (auto& t : tests) {
         auto result = dbms::mergeJoin(t.left, t.right, t.leftColNames, t.rightColNames);
-
-        // sortDataInColumns(result.columns);
 
         Assert(result.names.colNames == t.expected.names.colNames, "Column names mismatch");
         Assert(result.names.valueColNames == t.expected.names.valueColNames, "Value column names mismatch");
